d6a2b4e9eb9b18bb045087543acd8f98920a5b67
Tim Sollbach <tim.sollbach@grapes.de>
2023-06-14 16:27:10 +0200

    fix: background image cover size and mobile position

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-24 bg-left-top bg-no-repeat bg-landing-mobile lg:bg-landing lg:bg-right-top">
-     <div className="relative flex place-items-center">
+    <main className="flex flex-col items-center justify-center min-h-screen p-24 bg-left-top bg-no-repeat bg-cover bg-landing-mobile lg:bg-landing lg:bg-right-top">
+     <div className="relative flex place-items-center bottom-40">
         <Image
           className="relative"
           src="/logo.svg"
